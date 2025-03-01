d0b021c5edb811ca302d25a5aa73222768fe47dd
Amidamaroo <levchenkom@graveofhope.ru>
2025-03-01 12:12:42 +0300

    modified:   Builder/assets/startup.sh modified:   Builder/packages.py deleted:    config/alacritty/alacritty.toml deleted:    config/alacritty/themes/custom.toml modified:   config/bspwm/bspwmrc modified:   config/fish/config.fish modified:   config/fish/fish_variables modified:   config/gtk-3.0/settings.ini modified:   config/gtk-4.0/settings.ini modified:   config/mimeapps.list modified:   config/polybar/colors.ini modified:   config/polybar/config.ini modified:   config/polybar/launch.sh modified:   config/polybar/modules.ini modified:   config/sxhkd/sxhkdrc modified:   bin/polybar-wireguard (modified content)

--- Builder/packages.py
+++ Builder/packages.py
@@ -1,31 +1,31 @@
 
 
 BASE_PACKAGES = [
-    "tumbler", "ffmpegthumbnailer", "lsd", "alacritty", "bat", "brightnessctl", "calc",
-    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh", "firefox",
+    "tumbler", "ffmpegthumbnailer", "lsd", "kitty","ibat", "brightnessctl", "calc",
+    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh", 
     "fish", "dpkg", "gcc", "gedit", "git", "gnu-netcat", "htop", "btop", "nano", "lxappearance",
     "mat2", "mpd", "mpv", "thunar", "ncmpcpp", "neofetch", "network-manager-applet", "nitrogen",
     "pamixer", "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc", "pulseaudio",
-    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "rofi-calc", "calcurse",
+    "pulseaudio-alsa", "python-pyalsa", "yazi","reflector", "rofi", "rofi-calc", "calcurse",
     "rofi-emoji", "scrot", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "xarchiver",
     "xfce4-power-manager", "xfce4-settings", "xorg-xbacklight", "zathura", "zathura-djvu", "zathura-pdf-mupdf",
     "cmake", "clang", "gzip", "imagemagick", "make", "openssh", "pulseaudio-bluetooth", "shellcheck",
     "vlc", "usbutils", "picom", "networkmanager-openvpn", "alsa-plugins", "alsa-tools", "alsa-utils", "ffmpeg",
     "p7zip", "gparted", "sshfs", "openvpn", "xclip", "gpick", "wget", "ueberzug", "netctl", "light", "libreoffice",
     "breeze", "vulkan-intel", "intel-ucode", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
-    "ttf-iosevka-nerd"
+    "ttf-iosevka-nerd", "otf-codenewroman-nerd", "firefox", "vim", "lolcat","cowfortune", "ttf-anonymouspro-nerd"
 ]
 
 DEV_PACKAGES = [
     "cheese", "screenkey", "timeshift", "pinta", "kdenlive", "wireshark-qt",
-    "filezilla", "ghex", "chromium", "keepassxc", "audacity", "gufw", "python-pywal",
-    "bleachbit", "veracrypt", "homebank", "gtkhash", "gnome-firmware", "touche", "dconf-editor",
-    "neovim", "obs-studio", "telegram-desktop", "tmux", "youtube-dl", "code", "cowsay",
-    "deluge-gtk", "flameshot", "sqlitebrowser", "obsidian", "python-pip", "bpython", "ipython", "cloc",
+    "ghex", "chromium", "audacity", "gufw", 
+    "bleachbit","gtkhash", "gnome-firmware", "touche", "dconf-editor",
+    "neovim", "obs-studio", "telegram-desktop", "tmux","cowsay",
+    "deluge-gtk", "flameshot", "obsidian"
 ]
 
 AUR_PACKAGES = [
-    "cava", "i3lock-color", "ptpython"
+    "cava", "i3lock-color", "vim-plug"
 ]
 
 GNOME_OFFICIAL_TOOLS = [
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,9 @@
 
 # Create aliases
-alias cls="clear"
-alias g="git"
-alias n="nvim"
 alias m="micro"
-
+alias ls="exa -a --icons"
+alias v="vim"
+alias update="yay && kitten notify "обновились" && shutdown && cmatrix"
 # TODO: Replace journal aliases after switching to OpenRC
 
 # Display critical errors
@@ -24,11 +23,25 @@ alias vacuum_time="journalctl --vacuum-time=2weeks"
 
 set -U fish_greeting
 set fish_color_command green
-set -gx EDITOR micro
-set -gx VISUAL micro
-set -gx BROWSER /usr/bin/firefox
+set -gx EDITOR vim 
+set -gx VISUAL vim 
+set -gx BROWSER /usr/bin/floorp
 
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+#ansi картинка
+cowfortune | lolcat -t -F 1
+
+#yazi 
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
